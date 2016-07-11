3954b44f2682eb9df4a5bdbc8b77bed58e2e9af6
Zhao <jzhao@imshealth.com>
2016-07-11 08:02:19 +0100

    for test the bug fixing for seed setting

--- scripts/run__BooleanPredictor.R
+++ scripts/run__BooleanPredictor.R
@@ -13,12 +13,12 @@ main.arglist$kFoldsVal <- 3
 main.arglist$alphaVals <- seq(0,1,length.out=2)
 main.arglist$log_lambda_seq <- seq(log(1e-4),log(1e4),length.out=10)
 main.arglist$bClassWeights <- T
-main.arglist$n_repeats <- 1
+main.arglist$n_repeats <- 3
 
 main.arglist$data_dir <- "F:/Lichao/work/Projects/MultipleSclerosis/Results/2016-07-07/2016-07-07 16.01.54/"
-main.arglist$outcomes <- c("relapse_fu_any_01", "edssprog", "edssconf3",
-                   "relapse_or_prog", "relapse_and_prog", "relapse_or_conf")
-# main.arglist$outcomes <- c("relapse_fu_any_01")
+# main.arglist$outcomes <- c("relapse_fu_any_01", "edssprog", "edssconf3",
+#                    "relapse_or_prog", "relapse_and_prog", "relapse_or_conf")
+main.arglist$outcomes <- c("relapse_fu_any_01")
 
 main.arglist$data_names <- c("Cmp")
 
