554d69408b2414b185a7ec1cf43b233506102ea3
Zhao <jzhao@imshealth.com>
2016-07-13 02:48:01 +0100

    use Lichao's parameter setting to get the initial result on July12

--- scripts/run__BooleanPredictor.R
+++ scripts/run__BooleanPredictor.R
@@ -8,17 +8,17 @@ main.arglist <- list()
 main.arglist$bParallel <- F
 main.arglist$nCores2Use <- detectCores() - 1
 
-main.arglist$kFoldsEval <- 3
-main.arglist$kFoldsVal <- 3
-main.arglist$alphaVals <- seq(0,1,length.out=2)
-main.arglist$log_lambda_seq <- seq(log(1e-4),log(1e4),length.out=10)
+main.arglist$kFoldsEval <- 5
+main.arglist$kFoldsVal <- 5
+main.arglist$alphaVals <- seq(0,1,length.out=20)
+main.arglist$log_lambda_seq <- seq(log(1e-4),log(1e4),length.out=100)
 main.arglist$bClassWeights <- T
-main.arglist$n_repeats <- 3
+main.arglist$n_repeats <- 1
 
-main.arglist$data_dir <- "F:/Lichao/work/Projects/MultipleSclerosis/Results/2016-07-07/2016-07-07 16.01.54/"
-# main.arglist$outcomes <- c("relapse_fu_any_01", "edssprog", "edssconf3",
-#                    "relapse_or_prog", "relapse_and_prog", "relapse_or_conf")
-main.arglist$outcomes <- c("relapse_fu_any_01")
+main.arglist$data_dir <- "F:\\Lichao\\work\\Projects\\MultipleSclerosis\\Results\\2016-07-11\\2016-07-11 20.07.54\\mergeCateg\\"
+main.arglist$outcomes <- c("relapse_fu_any_01", "edssprog", "edssconf3",
+                           "relapse_or_prog", "relapse_and_prog", "relapse_or_conf")
+# main.arglist$outcomes <- c("relapse_fu_any_01")
 
 main.arglist$data_names <- c("Cmp")
 
