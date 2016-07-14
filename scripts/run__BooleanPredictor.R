f531e13fb728d0513c545746885674b76e1691fb
Lichao Wang kgxsapp100 <lichao.wang@uk.imshealth.com>
2016-07-14 15:42:26 +0100

    Using real grid + data to produce delivery for 15-July

--- scripts/run__BooleanPredictor.R
+++ scripts/run__BooleanPredictor.R
@@ -8,14 +8,14 @@ main.arglist <- list()
 main.arglist$bParallel <- F
 main.arglist$nCores2Use <- detectCores() - 1
 
-main.arglist$kFoldsEval <- 3
-main.arglist$kFoldsVal <- 3
-main.arglist$alphaVals <- seq(0,1,length.out=2)
-main.arglist$log_lambda_seq <- seq(log(1e-4),log(1e4),length.out=5)
+main.arglist$kFoldsEval <- 5
+main.arglist$kFoldsVal <- 5
+main.arglist$alphaVals <- seq(0,1,length.out=21)
+main.arglist$log_lambda_seq <- seq(log(1e-4),log(1e4),length.out=100)
 main.arglist$bClassWeights <- T
 main.arglist$n_repeats <- 1
 
-main.arglist$data_dir <- "F:\\Lichao\\work\\Projects\\MultipleSclerosis\\Results\\2016-07-12\\2016-07-12 15.43.48\\"
+main.arglist$data_dir <- "F:/Lichao/work/Projects/MultipleSclerosis/Results/2016-07-12/2016-07-12 15.43.48/"
 main.arglist$outcomeNames <- c("relapse_fu_any_01", "edssprog", "edssconf3",
                            "relapse_or_prog", "relapse_and_prog", "relapse_or_conf")
 # main.arglist$outcomes <- c("relapse_fu_any_01")
@@ -29,9 +29,10 @@ main.arglist$bManualCV <- F
 
 main.arglist$dataFileSuffix <- '4Model_VarChanged.csv'
 
-main.arglist$bTopVarsOnly <- F
+main.arglist$bTopVarsOnly <- T
 main.arglist$nTopVars <- 10
-main.arglist$initEnetDir <- NULL
+main.arglist$initEnetDir <- 
+  "F:/Lichao/work/Projects/MultipleSclerosis/Results/2016-07-14/2016-07-14 12.30.14/"
 
 global.seed <- 1
 set.seed(global.seed)
