d4afcd90bf5f8cc6831f1781f41a96837c2033a3
Zhao <jzhao@imshealth.com>
2016-07-21 06:48:10 +0100

    update to get the InitModel result with full varibles in model data

--- scripts/run__BooleanPredictor.R
+++ scripts/run__BooleanPredictor.R
@@ -15,7 +15,7 @@ main.arglist$log_lambda_seq <- seq(log(1e-4),log(1e4),length.out=100)
 main.arglist$bClassWeights <- T
 main.arglist$n_repeats <- 1
 
-main.arglist$data_dir <- "F:/Lichao/work/Projects/MultipleSclerosis/Results/2016-07-12/2016-07-12 15.43.48/"
+main.arglist$data_dir <- "F:/Jie/MS/03_Result/2016-07-21/2016-07-21 04.45.38/"
 main.arglist$outcomeNames <- c("relapse_fu_any_01", "edssprog", "edssconf3",
                            "relapse_or_prog", "relapse_and_prog", "relapse_or_conf")
 # main.arglist$outcomes <- c("relapse_fu_any_01")
@@ -27,9 +27,9 @@ main.arglist$idColName <- c("record_num")
 main.arglist$bTransferLearn <- F
 main.arglist$bManualCV <- F
 
-main.arglist$dataFileSuffix <- '4Model_VarChanged.csv'
+main.arglist$dataFileSuffix <- '4Model.csv'
 
-main.arglist$bTopVarsOnly <- T
+main.arglist$bTopVarsOnly <- F
 main.arglist$nTopVars <- 10
 main.arglist$initEnetDir <- 
   "F:/Lichao/work/Projects/MultipleSclerosis/Results/2016-07-14/2016-07-14 20.48.49/"
